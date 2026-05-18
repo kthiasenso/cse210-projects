34d46d425f5251f3e17f0857f2f2f5222139b70f
keventst <kevenpub.thia@gmail.com>
2026-05-19 00:16:11 +0200

    Ex 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is you grade? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (grade >= 80)
+        {
+            letter = "B";   
+        }
+
+        else if (grade >= 70)
+        {
+            letter = "C"; 
+        }
+
+        else if (grade >= 60)
+        {
+            letter = "D"; 
+        }
+        
+        else
+        {
+            letter = "F"; 
+        }
+
+        Console.WriteLine($"With a grade of {grade} you have an {letter}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulation you passed the course! ");
+        }
+        else
+        {
+             Console.WriteLine("Better luck next time, study hard and you'll be able to do it. ");
+        }
     }
 }
