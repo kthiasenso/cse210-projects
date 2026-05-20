b03ba58b309c27f69882cb0a0aa874fa04a3f5f5
keventst <kevenpub.thia@gmail.com>
2026-05-21 00:06:04 +0200

    ex 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string response;
+        
+        do
+        {
+            Console.WriteLine("Hello and Welcome to the 'Guess the number'! ");
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 101);
+            
+            int guess = 0;
+            while (guess != number)
+            {
+                Console.Write("What is your guess? ");
+                string userInput = Console.ReadLine();
+                guess = int.Parse(userInput);
+                if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You found it! ");
+                }
+            }
+            
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+        } while (response == "yes");
+        
     }
 }
